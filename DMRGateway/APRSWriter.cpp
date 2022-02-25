1ab91939f6a37bec56af76c1fc9426352492dc3a
Chipster <go@w.ay>
2022-02-25 08:37:54 -0600

    Update sources

--- DMRGateway/APRSWriter.cpp
+++ DMRGateway/APRSWriter.cpp
@@ -156,7 +156,7 @@ void CAPRSWriter::sendIdFrame()
 		server.append("S");
 
 	char output[500U];
-	::sprintf(output, "%s>APDG03,TCPIP*,qAC,%s:!%s%cD%s%c&/A=%06.0f%s %s\r\n",
+	::sprintf(output, "%s>APDG03,TCPIP*,qAC,%s:!%s%cR%s%c&/A=%06.0f%s %s\r\n",
 		m_callsign.c_str(), server.c_str(),
 		lat, (m_latitude < 0.0F)  ? 'S' : 'N',
 		lon, (m_longitude < 0.0F) ? 'W' : 'E',
