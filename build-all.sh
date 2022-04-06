30cbff690e8c8b9fa01415ff8f84b644ce5a968e
Chipster <go@w.ay>
2022-04-05 20:01:12 -0500

    improve build script

--- build-all.sh
+++ build-all.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
-ROOT_DIR=$HOME/dev/WPSD_CustomBinaries-Source
+SRC_DIR=$HOME/dev/WPSD_CustomBinaries-Source
+DEST_DIR=$HOME/dev/W0CHP-PiStar-bin
 
-cd $ROOT_DIR/APRSGateway ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/MMDVMHost ; make clean ; make -j4 ; make install ; make clean make -j4 -f Makefile.Pi.Adafruit ; make install ; make clean
-cd $ROOT_DIR/DAPNETGateway ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/DMRGateway ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/DStarRepeater ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/ircDDBGateway ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/M17Gateway ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/MMDVMCal ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/MMDVM_CM/DMR2YSF ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/MMDVM_CM/DMR2NXDN ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/MMDVM_CM/YSF2DMR ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/MMDVM_CM/YSF2P25 ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/MMDVM_CM/YSF2NXDN ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/MMDVM_CM/NXDN2DMR ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/NXDNClients ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/P25Clients ; make clean ; make -j4 ; make install ; make clean
-cd $ROOT_DIR/YSFClients ; make clean ; make -j4 ; make install ; make clean
+cd $SRC_DIR/APRSGateway ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/MMDVMHost ; make clean ; make -j5 ; make install ; make clean ; \
+     make -j5 -f Makefile.Pi.Adafruit ; make -f Makefile.Pi.Adafruit install ; make -f Makefile.Pi.Adafruit clean
+cd $SRC_DIR/DAPNETGateway ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/DMRGateway ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/DStarRepeater ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/ircDDBGateway ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/M17Gateway ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/MMDVMCal ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/MMDVM_CM/DMR2YSF ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/MMDVM_CM/DMR2NXDN ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/MMDVM_CM/YSF2DMR ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/MMDVM_CM/YSF2P25 ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/MMDVM_CM/YSF2NXDN ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/MMDVM_CM/NXDN2DMR ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/NXDNClients ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/P25Clients ; make clean ; make -j5 ; make install ; make clean
+cd $SRC_DIR/YSFClients ; make clean ; make -j5 ; make install ; make clean
 
-strip -v `find -type f -executable -exec file -i '{}' \; | grep 'x-executable; charset=binary' | sed 's/:.*//g'`
+strip `find $DEST_DIR -type f -executable -exec file -i '{}' \; | grep 'x-executable; charset=binary' | sed 's/:.*//g'`
