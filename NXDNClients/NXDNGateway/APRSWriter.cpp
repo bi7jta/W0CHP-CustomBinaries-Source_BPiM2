89a6931124e3d7c08886646fa296b9044742a010
Chipster <go@w.ay>
2022-04-08 18:31:48 -0500

    Update APRS strings

--- NXDNClients/NXDNGateway/APRSWriter.cpp
+++ NXDNClients/NXDNGateway/APRSWriter.cpp
@@ -173,12 +173,12 @@ void CAPRSWriter::sendIdFrameFixed()
 	char desc[200U];
 	if (m_txFrequency != 0U) {
 		float offset = float(int(m_rxFrequency) - int(m_txFrequency)) / 1000000.0F;
-		::sprintf(desc, "- https://w0chp.net/w0chp-pistar-dash/ %.5LfMHz %c%.4lfMHz%s%s",
+		::sprintf(desc, "%.5LfMHz %c%.4lfMHz%s%s - https://w0chp.net/w0chp-pistar-dash/",
 			(long double)(m_txFrequency) / 1000000.0F,
 			offset < 0.0F ? '-' : '+',
 			::fabs(offset), m_desc.empty() ? "" : "; ", m_desc.c_str());
 	} else {
-		::sprintf(desc, "- W0CHP-PiStar-Dash%s%s", m_desc.empty() ? "" : "; ", m_desc.c_str());
+		::sprintf(desc, "%s%s - https://w0chp.net/w0chp-pistar-dash/", m_desc.empty() ? "" : "; ", m_desc.c_str());
 	}
 
 	const char* band = "4m";
@@ -266,12 +266,12 @@ void CAPRSWriter::sendIdFrameMobile()
 	char desc[200U];
 	if (m_txFrequency != 0U) {
 		float offset = float(int(m_rxFrequency) - int(m_txFrequency)) / 1000000.0F;
-		::sprintf(desc, "- https://w0chp.net/w0chp-pistar-dash/ %.5LfMHz %c%.4lfMHz%s%s",
+		::sprintf(desc, "%.5LfMHz %c%.4lfMHz%s%s - https://w0chp.net/w0chp-pistar-dash/",
 			(long double)(m_txFrequency) / 1000000.0F,
 			offset < 0.0F ? '-' : '+',
 			::fabs(offset), m_desc.empty() ? "" : "; ", m_desc.c_str());
 	} else {
-		::sprintf(desc, "- https://w0chp.net/w0chp-pistar-dash/%s%s", m_desc.empty() ? "" : "; ", m_desc.c_str());
+		::sprintf(desc, "%s%s - https://w0chp.net/w0chp-pistar-dash/", m_desc.empty() ? "" : "; ", m_desc.c_str());
 	}
 
 	const char* band = "4m";
