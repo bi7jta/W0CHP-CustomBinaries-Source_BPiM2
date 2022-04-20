3bae02e83660b6125107f0bb2e73fa3c6716c2ec
Chipster <go@w.ay>
2022-04-20 08:13:17 -0500

    Quiet logs a bit to prevent erroenous "Not Linked" status.

--- NXDNClients/NXDNGateway/UDPSocket.cpp
+++ NXDNClients/NXDNGateway/UDPSocket.cpp
@@ -29,7 +29,7 @@
 #include "Log.h"
 #else
 #define LogMessage(fmt, ...)	::fprintf(stderr, fmt "\n", ## __VA_ARGS__)
-#define LogError(fmt, ...)	::fprintf(stderr, fmt "\n", ## __VA_ARGS__)
+#define LogDebug(fmt, ...)	::fprintf(stderr, fmt "\n", ## __VA_ARGS__)
 #define LogInfo(fmt, ...)	::fprintf(stderr, fmt "\n", ## __VA_ARGS__)
 #endif
 
@@ -69,7 +69,7 @@ void CUDPSocket::startup()
 	WSAData data;
 	int wsaRet = ::WSAStartup(MAKEWORD(2, 2), &data);
 	if (wsaRet != 0)
-		LogError("Error from WSAStartup");
+		LogDebug("Error from WSAStartup");
 #endif
 }
 
@@ -103,7 +103,7 @@ int CUDPSocket::lookup(const std::string& hostname, unsigned short port, sockadd
 		paddr->sin_family = AF_INET;
 		paddr->sin_port = htons(port);
 		paddr->sin_addr.s_addr = htonl(INADDR_NONE);
-		LogError("Cannot find address for host %s", hostname.c_str());
+		LogDebug("Cannot find address for host %s", hostname.c_str());
 		return err;
 	}
 
@@ -184,7 +184,7 @@ bool CUDPSocket::open(const unsigned int index, const unsigned int af, const std
 	/* to determine protocol family, call lookup() first. */
 	int err = lookup(address, port, addr, addrlen, hints);
 	if (err != 0) {
-		LogError("The local address is invalid - %s", address.c_str());
+		LogDebug("The local address is invalid - %s", address.c_str());
 		return false;
 	}
 
@@ -193,9 +193,9 @@ bool CUDPSocket::open(const unsigned int index, const unsigned int af, const std
 	int fd = ::socket(addr.ss_family, SOCK_DGRAM, 0);
 	if (fd < 0) {
 #if defined(_WIN32) || defined(_WIN64)
-		LogError("Cannot create the UDP socket, err: %lu", ::GetLastError());
+		LogDebug("Cannot create the UDP socket, err: %lu", ::GetLastError());
 #else
-		LogError("Cannot create the UDP socket, err: %d", errno);
+		LogDebug("Cannot create the UDP socket, err: %d", errno);
 #endif
 		return false;
 	}
@@ -209,18 +209,18 @@ bool CUDPSocket::open(const unsigned int index, const unsigned int af, const std
 		int reuse = 1;
 		if (::setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) == -1) {
 #if defined(_WIN32) || defined(_WIN64)
-			LogError("Cannot set the UDP socket option, err: %lu", ::GetLastError());
+			LogDebug("Cannot set the UDP socket option, err: %lu", ::GetLastError());
 #else
-			LogError("Cannot set the UDP socket option, err: %d", errno);
+			LogDebug("Cannot set the UDP socket option, err: %d", errno);
 #endif
 			return false;
 		}
 
 		if (::bind(fd, (sockaddr*)&addr, addrlen) == -1) {
 #if defined(_WIN32) || defined(_WIN64)
-			LogError("Cannot bind the UDP address, err: %lu", ::GetLastError());
+			LogDebug("Cannot bind the UDP address, err: %lu", ::GetLastError());
 #else
-			LogError("Cannot bind the UDP address, err: %d", errno);
+			LogDebug("Cannot bind the UDP address, err: %d", errno);
 #endif
 			return false;
 		}
@@ -259,9 +259,9 @@ int CUDPSocket::read(unsigned char* buffer, unsigned int length, sockaddr_storag
 #endif
 	if (ret < 0) {
 #if defined(_WIN32) || defined(_WIN64)
-		LogError("Error returned from UDP poll, err: %lu", ::GetLastError());
+		LogDebug("Error returned from UDP poll, err: %lu", ::GetLastError());
 #else
-		LogError("Error returned from UDP poll, err: %d", errno);
+		LogDebug("Error returned from UDP poll, err: %d", errno);
 #endif
 		return -1;
 	}
@@ -289,9 +289,9 @@ int CUDPSocket::read(unsigned char* buffer, unsigned int length, sockaddr_storag
 #endif
 	if (len <= 0) {
 #if defined(_WIN32) || defined(_WIN64)
-		LogError("Error returned from recvfrom, err: %lu", ::GetLastError());
+		LogDebug("Error returned from recvfrom, err: %lu", ::GetLastError());
 #else
-		LogError("Error returned from recvfrom, err: %d", errno);
+		LogDebug("Error returned from recvfrom, err: %d", errno);
 
 		if (len == -1 && errno == ENOTSOCK) {
 			LogMessage("Re-opening UDP port on %hu", m_port[index]);
@@ -326,9 +326,9 @@ bool CUDPSocket::write(const unsigned char* buffer, unsigned int length, const s
 
 		if (ret < 0) {
 #if defined(_WIN32) || defined(_WIN64)
-			LogError("Error returned from sendto, err: %lu", ::GetLastError());
+			LogDebug("Error returned from sendto, err: %lu", ::GetLastError());
 #else
-			LogError("Error returned from sendto, err: %d", errno);
+			LogDebug("Error returned from sendto, err: %d", errno);
 #endif
 		} else {
 #if defined(_WIN32) || defined(_WIN64)
