4c0bb150411347d41721a9cbeaccf12ced7bc668
Chipster <go@w.ay>
2022-10-03 07:58:07 -0500

    * Minor: rebuild and sync

--- MMDVMCal/Version.h
+++ MMDVMCal/Version.h
@@ -19,6 +19,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-#define VERSION "20221001_W0CHP-PiStar-Dash"
+#define VERSION "20221003_W0CHP-PiStar-Dash"
 
 #endif
--- NXDNClients/NXDNParrot/Version.h
+++ NXDNClients/NXDNParrot/Version.h
@@ -19,6 +19,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20221001_W0CHP-PiStar-Dash";
+const char* VERSION = "20221003_W0CHP-PiStar-Dash";
 
 #endif
