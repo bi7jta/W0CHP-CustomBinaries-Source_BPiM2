172b4493ac6cf0ba2a2c6703bd142efb7623b472
Chipster <go@w.ay>
2022-10-08 12:41:46 -0500

    * Minor: APRS symbol tweak and version bump

--- MMDVM_CM/YSF2NXDN/APRSWriter.cpp
+++ MMDVM_CM/YSF2NXDN/APRSWriter.cpp
@@ -196,7 +196,7 @@ void CAPRSWriter::sendIdFrames()
 		server.append("S");
 
         char output[1500U];
-        ::sprintf(output, "%s>APDG03,TCPIP*,qAC,%s:!%s%cW%s%cW/A=%06.0f%s %s\r\n%s>APDG03:>Pwrd. by W0CHP-PiStar-Dash (https://wpsd.w0chp.net)\r\n",                                               
+        ::sprintf(output, "%s>APDG03,TCPIP*,qAC,%s:!%s%cW%s%c#/A=%06.0f%s %s\r\n%s>APDG03:>Pwrd. by W0CHP-PiStar-Dash (https://wpsd.w0chp.net)\r\n",                                               
                 m_callsign.c_str(), server.c_str(),
                 lat, (m_latitude < 0.0F)  ? 'S' : 'N',
                 lon, (m_longitude < 0.0F) ? 'W' : 'E',
